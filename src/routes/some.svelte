72d31b649cf6121b81478058770923ad1dbfe79a
Iltimas Doha <iltimasd@users.noreply.github.com>
2021-08-02 16:28:09 -0400

    first commit

---
+++ src/routes/some.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { fade } from "svelte/transition";
+  import { onMount } from "svelte";
+
+  let mounted;
+  onMount(() => {
+    mounted = true;
+  });
+</script>
+
+<input type="checkbox" bind:checked={mounted} />
+{#if mounted}
+  <div in:fade|local={{ duration: 1000, delay: 1000 }}>
+    <h2>maybe nesting will help?</h2>
+  </div>
+  <h1 in:fade|local={{ duration: 1000, delay: 2000 }}>headers</h1>
+  <h3 in:fade|local={{ duration: 1000, delay: 2250 }}>all of them</h3>
+  <input in:fade|local={{ duration: 1000, delay: 2500 }} />
+  <div in:fade|local={{ duration: 1000, delay: 2750 }}>plain div</div>
+{/if}
